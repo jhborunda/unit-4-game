048777b7e66bc72fcff173607b8fc419ab21aeca
Jose Borunda <jose.h.borunda@gmail.com>
2019-07-14 18:33:39 -0600

    completed html and startred JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+// Randon Number between 19 & 20
+var Random=Math.floor(Math.random()*101+19);
+
+$('#randomNumber').text(Random);
+
+//crystal's hidden value between 1-12
+
+
+})
