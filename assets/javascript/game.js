30a7cbafe2595c13803ec1b9ded6da76219d7e97
Jose Borunda <jose.h.borunda@gmail.com>
2019-07-14 18:38:20 -0600

    started pseudo coding

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,5 +6,11 @@ $('#randomNumber').text(Random);
 
 //crystal's hidden value between 1-12
 
+//create variables for crystals
 
+//number to be pushed for winsLosses
+
+//clicks for gems
+
+//reset
 })
